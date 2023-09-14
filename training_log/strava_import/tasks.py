d36f35af4d7f7258653a593650dcff9c8a82c890
Alex Hartog <hartog.alex@gmail.com>
2023-09-14 12:19:06 +0200

    Changed crontab to run every hour at xx:29

--- training_log/strava_import/tasks.py
+++ training_log/strava_import/tasks.py
@@ -3,6 +3,6 @@ from huey.contrib.djhuey import periodic_task
 from strava_import.strava import strava_sync
 
 
-@periodic_task(crontab(hour="*/1"))
+@periodic_task(crontab(minute="29"))
 def strava_sync_task():
     strava_sync()
